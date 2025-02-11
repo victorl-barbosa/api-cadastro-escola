5fb34632469c504acf2c631629f1f2bc85e188b4
victor barbosa <contato.victorlb@gmail.com>
2025-02-11 11:33:12 -0300

    Tratando os arquivos enviados

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,20 @@
+import multer from 'multer'
+import multerConfig from '../config/multerConfig'
+
+const upload = multer(multerConfig).single('foto')
+
+
 class FotoController {
-  async store(req,res) {
-    res.json(req.file)
+  async store(req, res) {
+    return upload(req, res, (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code]
+        })
+      }
+
+      return res.json(req.file)
+    })
   }
 }
 
